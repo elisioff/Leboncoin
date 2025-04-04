df5c27f2d9efdae2c524036523a67926f0efa29a
Elísio Fernandes <leak.bronco_0g@icloud.com>
2025-04-04 09:35:03 +0100

    Added project.

---
+++ Leboncoin/Leboncoin/LeboncoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeboncoinApp.swift
+//  Leboncoin
+//
+//  Created by Elísio Fernandes on 04/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct LeboncoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
