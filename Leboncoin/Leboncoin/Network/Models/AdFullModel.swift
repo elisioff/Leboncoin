6241bd5c7c253f72b2ab865c2b3a36f77089650d
Elísio Fernandes <leak.bronco_0g@icloud.com>
2025-04-04 09:47:18 +0100

    Added Networkd models. Set iOS minimum versions. Removed visionOS and macOS support.

---
+++ Leboncoin/Leboncoin/Network/Models/AdFullModel.swift
@@ -0,0 +1,33 @@
+//
+//  AdFullModel.swift
+//  Leboncoin
+//
+//  Created by Elísio Fernandes on 04/04/2025.
+//
+
+import Foundation
+
+protocol AdSimpleModel {
+    var imagesUrl: ImageModel { get }
+    var categoryId: Int { get }
+    var title: String { get }
+    var price: Double { get }
+    var isUrgent: Bool { get }
+}
+
+struct AdFullModel: Decodable, AdSimpleModel {
+    let id: Int
+    let title: String
+    let categoryId: Int
+    let creationDate: String
+    let description: String
+    let isUrgent: Bool
+    let imagesUrl: ImageModel
+    let price: Double
+    let siret: String?
+}
+
+struct ImageModel: Decodable {
+    let small: String
+    let thumb: String
+}
